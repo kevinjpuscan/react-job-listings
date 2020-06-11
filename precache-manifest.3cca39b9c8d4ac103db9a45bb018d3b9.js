ad3e1b737ee0f8dd7ced58a0c12a2e246e8ca776
kevin puscan <kevinjpuscan@gmail.com>
2020-06-11 18:32:37 -0500

    Updates

---
+++ precache-manifest.3cca39b9c8d4ac103db9a45bb018d3b9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66fc9ea4b7061f830b83efc9a5651569",
+    "url": "/react-job-listings/index.html"
+  },
+  {
+    "revision": "477cb20f6977a37e220e",
+    "url": "/react-job-listings/static/css/main.a288ec06.chunk.css"
+  },
+  {
+    "revision": "1e1cdcc1bf5a20c2402e",
+    "url": "/react-job-listings/static/js/2.930024af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-job-listings/static/js/2.930024af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477cb20f6977a37e220e",
+    "url": "/react-job-listings/static/js/main.7e4247c2.chunk.js"
+  },
+  {
+    "revision": "29ca4f65f78a2fb39557",
+    "url": "/react-job-listings/static/js/runtime-main.72a81363.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/react-job-listings/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/react-job-listings/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/react-job-listings/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "437102d8083ab6ef8b3db05cbab6f885",
+    "url": "/react-job-listings/static/media/close.437102d8.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/react-job-listings/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/react-job-listings/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/react-job-listings/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/react-job-listings/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/react-job-listings/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/react-job-listings/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/react-job-listings/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/react-job-listings/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/react-job-listings/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
